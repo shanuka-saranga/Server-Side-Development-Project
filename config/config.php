092e0da43172fa0082be1f023e6b22bf17b73a60
shanuka-saranga <shanukasaranga974@gmail.com>
2025-10-30 00:55:26 +0530

    Clean up config.php by removing commented lines for improved readability

--- config/config.php
+++ config/config.php
@@ -1,14 +1,11 @@
 <?php
-// Database connection
-$servername = "localhost";    // usually localhost
-$username = "root";           // your DB username
-$password = "";               // your DB password
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "festora_db";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
