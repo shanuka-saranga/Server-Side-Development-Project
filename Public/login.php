76c6645eccb81ee8dd6ed5aa66ab8a5ab9e98b3f
shanuka-saranga <shanukasaranga974@gmail.com>
2025-10-29 22:05:11 +0530

    Fix require statement in login.php to use the correct config file

--- Public/login.php
+++ Public/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../config/database.php';
+require_once '../config/config.php';
 
 // ============== SIGNUP LOGIC ==============
 if (isset($_POST['signup'])) {
